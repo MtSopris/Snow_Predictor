675a8786c3e6a5b8d540410a4deb077a58cabb35
Anna Sours <acsours@gmail.com>
2021-05-25 21:19:39 -0600

    flask app returns jsonified stations

--- app.py
+++ app.py
@@ -1,10 +1,26 @@
 from flask import Flask, request, jsonify
+import sqlalchemy
+from sqlalchemy.ext.automap import automap_base
+from sqlalchemy.orm import Session
+from sqlalchemy import create_engine
+import json
+
 # sqlalchemy
 # can upload information with credentials - I can access this db with credentials, any data we put into db is now available into 
 
 # heroku
 
+db_connection_string = "postgres://mnaxahwxxlsupb:82ba661b23abc055281f8b75926dee77b960380953d91b0529fe16e0ed78f832@ec2-54-163-97-228.compute-1.amazonaws.com:5432/dajiaraierf0ld"
+engine = create_engine(db_connection_string)
+station_name_array=engine.execute('SELECT station_name FROM station_table').fetchall()
+lat_lon=engine.execute('SELECT station_table.latitude, station_table.longitude FROM station_table').fetchall()
 
+# stations=[]
+# for each_station in station_name_array:
+#     station=each_station[0]
+#     stations.append(station)
+
+# print(lat_lon)
 
 app=Flask(__name__)
 
@@ -12,16 +28,27 @@ app=Flask(__name__)
 
 @app.route('/')
 def index():
+    # stations=[]
+    # for each_station in station_name_array:
+    #     station=each_station[0]
+    #     stations.append(station)
+    # print(f'first lat_lon {lat_lon[0]}')
+    # print(f'first station {stations[0]}')
     return('Welcome to the home page')
 
 
 
 @app.route('/predict')
 def predict():
+
+    return ('prediction from user input here')
 # use sqllite to access station names and latlongs 
     # grab input from form (or have user go to endpoint on their own)
 
     # loop over all possible stations, running our model on that input and each station
+    # for each_station in lat_lon:
+        # run model with input date (comes from form)
+
     # store output in shape (dict) along with station 
     # could optionally have list of lat longs that is stored here as well
 
@@ -44,12 +71,43 @@ def predict():
 
     # final dictionary
     # what do we want final dict look like? -> how does leaflet want a heatmap?
-    dummy_output={
+    # dummy_output={
+        # date
+        # station_name: ,
+        # lat_lon: ,
+        #  elevation: ,
+        # snowfall ,
+        # date?
 
-    }
-    return jsonify({'input'})
+    # }
+    # return jsonify({'input'})
+
+@app.route('/stations')
+def stations():
 
 
+    resultproxy = engine.execute('SELECT * FROM station_table')
+
+    d, a = {}, []
+    for rowproxy in resultproxy:
+        # rowproxy.items() returns an array like [(key0, value0), (key1, value1)]
+        for column, value in rowproxy.items():
+            # build up the dictionary
+            d ={**d, **{column: value}}
+        a.append(d)
+    return jsonify(a)
+
+    # lat_lon=engine.execute('SELECT DISTINCT lat, lon FROM snow_weather').fetchall()
+
+    # stations=[]
+    # for each_station in station_name_array:
+    #     station=each_station[0]
+    #     stations.append(station)
+    # return jsonify({"stations":stations})
+    # test_array=engine.execute('SELECT * FROM snow_weather LIMIT 5').fetchall() #station_table 
+    # # results = json.dumps({test_array})
+    # for each_result in test_array:
+    # return results
 
 if __name__=='__main__': 
 	app.run()
