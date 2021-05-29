9a9758f7974c404e47268a63636c23803844f544
LisaCaruana <75282802+LisaCaruana@users.noreply.github.com>
2021-05-29 14:15:20 -0600

    Bar chart/heatmap

--- static/js/heatmapv2.js
+++ static/js/heatmapv2.js
@@ -23,26 +23,38 @@ streetMap.addTo(myMap);
 var snow_url='../static/output/ml_predict_output.json' //hard code location for now
 
 //start d3.json
-d3.json(snow_url, function(response){
+d3.json(snow_url).then ((response=>{
   //console.log to see response
-  console.log(response)
+  // console.log(response)
+
+  var heatArray = []
+    response.forEach(function(one_station) {
+      // console.log(one_station.lat)
+      heatArray.push([one_station.lat, one_station.lon, one_station.predicted_snow])
+    })
+    // console.log(heatArray)
+
   //create empty list
-  var heatArray=[]
-  //start for loop
-  for(var i=0; i< response.length; i++) {
-    // not sure if best to use response[i] or reponse[i].station_name
-    var stationSelected = response[i].station_name;
-// added "predicted_snow"as the 3rd variable to give heat map a scale
-    if(stationSelected){
-      heatArray.push([station_name.lat,station_name.lon, station_name.predicted_snow]);
-    }
-  }
+//   var heatArray=[]
+//   //start for loop
+//   for(var i=0; i< response.length; i++) {
+//     // not sure if best to use response[i] or reponse[i].station_name
+//     var stationSelected = response[i].station_name;
+// // added "predicted_snow"as the 3rd variable to give heat map a scale
+//     if(stationSelected){
+//       heatArray.push([station_name.lat,station_name.lon, station_name.predicted_snow]);
+//     }
+//   }
 
   var heat = L.heatLayer(heatArray,{
           radius: 100,
           blur: 35,
           maxIntensity: 88,
           opacity: 1,
-          dissipating: True
+          // dissipating: True
     }).addTo(myMap)
-})
+}))
+
+// var heatArray = []
+//     data.forEach(function(one_sale) {
+//         heatArray.push([one_sale.lat, one_sale.lng, one_sale.num])
